03089bb946f99b9f791130567b133d5223182a2f
Raj Maharjan <rmaharjan.rm90@gmail.com>
2021-06-24 21:37:24 +0545

    Outputs of all added in README.MD

--- grouping.js
+++ grouping.js
@@ -6,63 +6,59 @@
 // 	  dell:[{}, {}, {}]
 // }
 
-function groupBy(obj, name){
-    return obj.reduce(
-        (arr, item) => {
-            if(typeof item[name]=="undefined"){
-                err = new Error("name: Undefined Error,. 'name' doesn't map with any value of object.");
-                throw err;
-            }
-            if(!arr.hasOwnProperty(item[name])){
-                arr[item[name]] = [item];
-            }else{
-                arr[item[name]].push(item);
-            }
-            return arr
-        },
-        {}
-    );
+function groupBy(obj, name) {
+  return obj.reduce((arr, item) => {
+    if (typeof item[name] == 'undefined') {
+      err = new Error(
+        "name: Undefined Error,. 'name' doesn't map with any value of object."
+      )
+      throw err
+    }
+    if (!arr.hasOwnProperty(item[name])) {
+      arr[item[name]] = [item]
+    } else {
+      arr[item[name]].push(item)
+    }
+    return arr
+  }, {})
 }
 
-mobileBrands = [{
-    name: "Samsung",
-    model: "Samsung Galaxy Note 10+ ",
-    display: 6.80,
-    os: "Android",
-    processor: "Samsung Exynos 9825"
-},
-{
-    name: "Samsung",
-    model: "Samsung Galaxy S21 Ultra",
-    display: 6.80,
-    os: "Android",
-    processor: "Samsung Exynos 2100"
-},
-{
-    name: "Apple",
-    model: "Apple iPhone 12 Pro Max",
-    display: 6.70,
-    os: "iOS",
-    processor: "A14 Bionic"
-},{
-    name: "Apple",
-    model: "Apple iPhone XS Max",
-    display: 6.50,
-    os: "iOS",
-    processor: "A12 Bionic"
-},{
-    name: "OnePlus",
-    model: "OnePlus 9 Pro",
-    display: 6.70,
-    os: "Android",
-    processor: "Qualcomm Snapdragon 888"
-},{
-    name: "Huawei",
-    model: "Huawei P30 Pro",
-    display: 6.47,
-    os: "Android",
-    processor: "HiSilicon Kirin 980"
-}
+mobileBrands = [
+  {
+    name: 'Samsung',
+    model: 'Samsung Galaxy Note 10+ ',
+    display: 6.8,
+    os: 'Android',
+    processor: 'Samsung Exynos 9825',
+  },
+  {
+    name: 'Samsung',
+    model: 'Samsung Galaxy S21 Ultra',
+    display: 6.8,
+    os: 'Android',
+    processor: 'Samsung Exynos 2100',
+  },
+  {
+    name: 'Apple',
+    model: 'Apple iPhone 12 Pro Max',
+    display: 6.7,
+    os: 'iOS',
+    processor: 'A14 Bionic',
+  },
+  {
+    name: 'Apple',
+    model: 'Apple iPhone XS Max',
+    display: 6.5,
+    os: 'iOS',
+    processor: 'A12 Bionic',
+  },
+  {
+    name: 'OnePlus',
+    model: 'OnePlus 9 Pro',
+    display: 6.7,
+    os: 'Android',
+    processor: 'Qualcomm Snapdragon 888',
+  },
 ]
 
-console.log("Grouped By ", groupBy(mobileBrands, "name"));
+console.log('Grouped By ', groupBy(mobileBrands, 'name'))
--- uniqueWithReduce.js
+++ uniqueWithReduce.js
@@ -1,14 +1,12 @@
 // perfrom your getUnique task with reduce
 
-var arr = [1, 2, 3, 4, 5, 6, 6 ,7 ,8 ,8 ,1 ,1 ,1];
+var arr = [1, 2, 3, 4, 5, 6, 6, 7, 8, 8, 1, 1, 1]
 
-var notRepeated = arr.reduce(
-	function(acc, item, index, source){
-		if(!acc.includes(item)){
-			acc.push(item)
-		}
-		return acc;
-	},
-	[]
-);
-console.log(notRepeated);
+var notRepeated = arr.reduce(function (acc, item, index, source) {
+  if (!acc.includes(item)) {
+    acc.push(item)
+  }
+  return acc
+}, [])
+console.log('The Repeated Array is ', arr)
+console.log('The Unique valued array is ', notRepeated)
